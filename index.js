8baac4234b08c05643a0fe145576b4d258373e0f
shamima-akter <shamimamirpur2@gmail.com>
2023-11-10 10:50:55 +0600

    get job data update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 1212;
@@ -27,10 +27,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const addJobCollection = client.db('jobStore').collection('addJob');
+
+    //add jobs
+    app.post('/addJob', async(req, res) =>{
+        const addJobs = req.body;
+        console.log(addJobs);
+        const result = await addJobCollection.insertOne(addJobs);
+        res.send(result);
+    })
+
+    app.get('/addjob', async(req, res) =>{
+        const cursor = addJobCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+// ------------------   new   ---------------------
+    app.get("/addjob/:id", async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)} // to find data by id from mongodb
+        const cursor = await addJobCollection.findOne(query)
+        res.send(cursor);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+  } catch {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
